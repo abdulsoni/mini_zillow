4958e8530f608b5d25b1b9dfb4189861b554d5f7
abdul <abdultipusultan@gmail.com>
2025-06-29 13:21:41 +0530

    added

--- server/index.js
+++ server/index.js
@@ -12,23 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/rental-app';
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  'http://localhost:3000', // for local dev
-  'https://mini-zillow-usr9.vercel.app'
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use(cors())
 
 app.use(express.json());
 
